df6becaf1d6fedbef414ab47c6097ecdce086a52
Kooreis <kooreis.official@gmail.com>
2022-09-13 18:00:00 -0500

    Added a line to convert the user's input into an integer and store it in a variable.

--- Leap_Year_Checker.cs
+++ Leap_Year_Checker.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter a year to check if it's a leap year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
     }
 }
